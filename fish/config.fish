c466ba8f3c3ed95a557ef5496098d952df45d4b4
RZZT <raulsanchezzt@duck.com>
2022-10-27 16:35:00 +0200

    🐟 Add some configurations

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,17 @@
+set fish_greeting ""
+
+# Aliases
+alias grep "grep --color=auto"
+alias cat "batcat --style=plain --paging=never"
+alias ls "exa --group-directories-first"
+alias tree "exa -T"
+
+alias gs "git status"
+alias gc "git commit"
+alias ga "git add"
+alias gp "git push"
+alias gl "gil pull"
+alias gd "git diff"
+
 # Start Starship
 starship init fish | source
