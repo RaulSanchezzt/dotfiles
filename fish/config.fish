bcbc7b6d38101f6c2a73ab4c0f6a4776a0920de3
RZZT <raulsanchezzt@duck.com>
2022-10-30 16:10:13 +0100

    🚈 Add more git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,14 @@ alias grep "grep --color=auto"
 alias cat "batcat --style=plain --paging=never"
 alias ls "exa --group-directories-first"
 alias tree "exa -T"
+alias blog "hugo server"
 
+alias gb "git add . && git commit -m 'Add work' && git push"
 alias gs "git status"
 alias gc "git commit"
 alias ga "git add"
 alias gp "git push"
-alias gl "gil pull"
+alias gl "git pull"
 alias gd "git diff"
 
 # Start Starship
