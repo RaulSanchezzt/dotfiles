9f1734e515c9bfbd71943f9017260921f698cf48
RZZT <raulsanchezzt@duck.com>
2022-11-27 17:04:46 +0100

    🦪 Add ASIR functions

--- fish/config.fish
+++ fish/config.fish
@@ -7,13 +7,32 @@ alias ls "exa --group-directories-first"
 alias tree "exa -T"
 alias blog "hugo server"
 
-alias gb "git add . && git commit -m 'Add work' && git push"
+# Git
 alias gs "git status"
 alias gc "git commit"
 alias ga "git add"
 alias gp "git push"
 alias gl "git pull"
 alias gd "git diff"
+alias gb "git add . && git commit -m 'Add work' && git push"
+
+# ASIR
+
+set blog /mnt/c/Users/Raul/Code/blog-asir/
+
+function cdb
+    cd $blog
+end
+
+function aso
+    cdb
+    cp templates/aso.md content/post/aso/index.md
+end
+
+function sadi
+    cdb
+    cp templates/sadi.md content/post/sadi/index.md
+end
 
 # Start Starship
 starship init fish | source
